932bec81d538748c1a09e1e83bfae902936cd967
mutegi <mutegisamuel0@gmail.com>
2017-12-01 16:02:41 +0300

    EDDITED SOME LINES OF CODE 	added some lines of code.

---
+++ Hangman.java
@@ -0,0 +1,28 @@
+package com.psl.main;
+
+public class Hangman {
+
+  static public void guess(String word, int life){
+
+    char[] filler = new char [word.length()];
+    int i=0;
+    while(i<word.length()){
+        filler[i]='-';
+        if(word.charAt(i)== ' '){
+          filler[i]=' ';
+        }
+        i++;
+    }
+    System.out.print 
+  }
+
+
+
+
+  public static void main(String[] args) {
+
+    String word ="welcome to Hangman"; //word to be guwssed
+    int life = 5;             //life or number of chances
+    guess(word,life);
+  }
+}
